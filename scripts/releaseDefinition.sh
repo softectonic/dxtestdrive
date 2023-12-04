7db038c8fc4e5220f66f5fd4612f3a7c13a245f6
softectonic <softectonic@gmail.com>
2023-12-04 01:47:38 +0000

    comments added

--- scripts/releaseDefinition.sh
+++ scripts/releaseDefinition.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Script: release-definition.sh
+# 
+# Description:
+# This script is responsible for generating the release definition file for the Salesforce deployment.
+# It uses parameters specified in 'release-vars-file.json' to create a tailored release definition,
+# which is used later in the deployment process. The script ensures that each release is 
+# accurately defined and recorded, facilitating a controlled and repeatable deployment process.
+#
+# Usage:
+# ./releaseDefinition.sh
+# 
+# The script reads required parameters from 'release-vars-file.json', such as the release name,
+# and then generates the release definition file accordingly. The generated file is saved in the
+# 'release-definitions' directory of the project.
+#
+# Note: This script should be executed in the context of a GitHub Actions workflow or
+# in an environment where all necessary dependencies and environment variables are set.
+#
+
 # Path to JSON file
 JSON_FILE="release-definitions/release-vars-file.json"
 
