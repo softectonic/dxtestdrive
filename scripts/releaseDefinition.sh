60b67014995a40b8962ead106981adcd9d87ed86
softectonic <softectonic@gmail.com>
2023-12-16 05:49:20 +0000

    release definition scripts update

--- scripts/releaseDefinition.sh
+++ scripts/releaseDefinition.sh
@@ -19,53 +19,41 @@
 # in an environment where all necessary dependencies and environment variables are set.
 #
 
-# Path to JSON file
-#JSON_FILE="release-definitions/release-vars-file.json"
-
-# Read variables from JSON file
-#RELEASE_NAME=$(jq -r '.releaseName' "$JSON_FILE")
-#RELEASE_BRANCH=$(jq -r '.releaseBranch' "$JSON_FILE")
-#RELEASE_DEFINITIONS_DIR=$(jq -r '.releaseDefinitionsDir' "$JSON_FILE")
-#SFDX_PROJECT_FILE=$(jq -r '.sfdxProjectFile' "$JSON_FILE")
-#SOURCE_TAG=$(jq -r '.sourceTag' "$JSON_FILE")
 
 # Default values
-today=$(date +"%m%d%Y")
-default_branch=$(git rev-parse --abbrev-ref HEAD)
-default_source_tag=$(git rev-parse HEAD)
-default_directory="release-definitions"
-default_config_file="${default_directory}/release-template.yml"
+default_config_file="${default_directory}/config.yml"
 
 # GitHub environment variables
-github_ref=${GITHUB_REF:-}
-github_sha=${GITHUB_SHA:-}
+##github_ref=${GITHUB_REF:-}
+#github_sha=${GITHUB_SHA:-}
 
 # Extract branch name from GITHUB_REF
-branch_name=""
-if [[ "$github_ref" == refs/heads/* ]]; then
-  branch_name=${github_ref#refs/heads/}
-fi
+#branch_name=""
+#if [[ "$github_ref" == refs/heads/* ]]; then
+#  branch_name=${github_ref#refs/heads/}
+#fi
 
 # Use provided values or defaults
-RELEASE_NAME=${1:-release-$today}
-DIRECTORY=${2:-$default_directory}
-CONFIG_FILE=${3:-$default_config_file}
-BRANCH=${branch_name}
-SOURCE_TAG=${github_sha}
-GIT_REF=${github_ref}
+CONFIG_FILE=${1:-$default_config_file}
+RELEASE_NAME="release"
+DIRECTORY="release-definition"
+BRANCH="release-definitions"
+SOURCE_TAG="main"
 
 
 # Check if releaseName is the placeholder and replace it with today's date
-if [ "$RELEASE_NAME" == "release-placeholder" ]; then
-    RELEASE_NAME="release-$today"
-fi
+#if [ "$RELEASE_NAME" == "release-placeholder" ]; then
+#    RELEASE_NAME="release-$today"
+#fi
 
 echo "Release Name: $RELEASE_NAME"
 echo "Branch: $BRANCH"
 echo "Directory: $DIRECTORY"
 echo "Config File: $CONFIG_FILE"
 echo "Source Tag: $SOURCE_TAG"
-echo "Git Ref: $GIT_REF"
+
+# Checkout to the new branch
+git checkout release-definition
 
 # Generate Release Definition
 sfp releasedefinition:generate -n "$RELEASE_NAME" -b "$BRANCH" -d "$DIRECTORY" -f "$CONFIG_FILE" -c "$SOURCE_TAG"
